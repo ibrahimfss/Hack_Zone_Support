832d8134899f9a2e4fd0c16a4ad197928ec6069c
ibrahimfss <sliprecord2@gmail.com>
2025-12-16 09:40:51 +0530

    Refactor welcome message in bot.js
    Updated welcome message format and added link to official channel.

--- api/bot.js
+++ api/bot.js
@@ -40,14 +40,13 @@ bot.start(async (ctx) => {
     IMAGES.WELCOME,
     {
       caption:
-`👋 *WELCOME, {first_name}!*
+`👋 *WELCOME, %firstname%!*
 
 You have successfully reached *HACK ZONE SUPPORT* 🛠️  
 Our team is here to assist you with all official support-related queries.
 
 ━━━━━━━━━━━━━━━━━━
-📢 *OFFICIAL CHANNEL*
-👉 @hack_zone_ai
+📢 *[OFFICIAL CHANNEL](t.me/hack_zone_ai)*
 ━━━━━━━━━━━━━━━━━━
 
 To continue and access support options, please click the *CONTINUE* button below.`,
