5d3dcfa7a6e2140cc021132b34fb72f84bd8e993
ibrahimfss <sliprecord2@gmail.com>
2025-12-16 16:05:17 +0530

    Update bot.js

--- api/bot.js
+++ api/bot.js
@@ -386,7 +386,7 @@ bot.action("PREDICTORS", (ctx) =>
     {
       type: "photo",
       media: IMAGES.PREDICTORS,
-      caption: `*🤖 Boost your earnings by using the Predictor & Hack bots below and increase your winning chances*.`,
+      caption: `🤖 *Boost your earnings by using the Predictor & Hack bots below and increase your winning chances*.`,
       parse_mode: "Markdown"
     },
     {
