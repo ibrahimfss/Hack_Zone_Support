b03542d2cfcfdf654471e1ed70e6f2d7b7a8f63a
ibrahimfss <sliprecord2@gmail.com>
2025-12-16 12:19:11 +0530

    Update WITHDRAW image URL in bot.js

--- api/bot.js
+++ api/bot.js
@@ -18,7 +18,7 @@ const bot = new Telegraf(BOT_TOKEN);
 const IMAGES = {
   WELCOME: "https://www.canva.com/design/DAG7pDLb09s/Cyyeg0YkySJBZU_nGawakQ/view?utm_content=DAG7pDLb09s&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h12f6f44082",
   MENU: "https://i.imgur.com/zYIlgBl.jpg",
-  WITHDRAW: "https://i.imgur.com/9ZQ3R0T.jpg",
+  WITHDRAW: "https://drive.google.com/file/d/1y-oc8TrDu229W37rOL7EbNcrs-rM-bh3/view?usp=drivesdk",
   DEPOSIT: "https://i.imgur.com/5rXkZ6K.jpg",
   BONUS: "https://i.imgur.com/6L89KkP.jpg",
   VOUCHER: "https://i.imgur.com/8QfGxwS.jpg",
